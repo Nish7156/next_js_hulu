87f9fecd02983dd08ef604080cc7fac90db210cb
Nish7156 <shedagenishant40@gmail.com>
2023-01-01 16:10:00 +0530

    test

--- pages/index.js
+++ pages/index.js
@@ -11,59 +11,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+     <h1>hiii</h1>
     </div>
   )
 }
+
+// export async function getServerSideProps() {
+
+//   const response= await fetch('http://localhost:1337/api/products', {
+//     method: 'GET',
+//     headers: {
+//       authorization: 'Bearer bda31179510b8b83233ce336d126178a1c5c7766ec53e317058fa9f82991fd65e0f00962ef9da14c46fe38a4c39530f16c0cb1cb8e0f8a95659c35b47f7cc4e4668b3cd26d6c56fd072a872f24c4a2a83e98d77426c20ae25aecb43b63af66e25d2f9c9544f545ab9de35b1661a161691b2acdc36b1350d7e824586dcd48364f'
+//     },
+//   });
+//   const data = await response.json()
+//   console.log(data.data,"data");
+//   return {
+//     props: {
+//       data:[]
+//     }, // will be passed to the page component as props
+//   }
+// }
